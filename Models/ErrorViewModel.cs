652bccd7050153d8f4714e3669ae46e9a70a9c44
Stiven Fortes <stiven.fortes@devgo.org>
2023-03-16 16:53:15 -0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LeaveManagmentApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
